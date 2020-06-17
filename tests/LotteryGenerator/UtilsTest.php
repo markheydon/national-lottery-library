6e752566a144a4394e3aa75b10ed7e39a1b81ca6
Mark Heydon <markheydon@mhcg.co.uk>
2020-06-17 00:04:05 +0000

    tempnam() doesn't work on Windows.

--- tests/LotteryGenerator/UtilsTest.php
+++ tests/LotteryGenerator/UtilsTest.php
@@ -38,6 +38,7 @@ class UtilsTest extends TestCase
         $line1 = "val1,val2" . PHP_EOL;
         $line2 = "val1,val2" . PHP_EOL;
 
+        // TODO: #13 Fix tempnam() call on Windows platforms.
         $tempFile = tempnam('/tmp', 'test-csv-');
         $temp = fopen($tempFile, 'w');
         fwrite($temp, $header);
